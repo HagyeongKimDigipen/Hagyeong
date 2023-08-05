60831cdf942a9af2c000a0d9ab65097c0eb40a67
Hagyeong Kim <101783334+HagyeongKimDigipen@users.noreply.github.com>
2023-08-05 00:30:17 -0700

    16

--- js/freelancer.js
+++ js/freelancer.js
@@ -35,3 +35,26 @@ $('body').scrollspy({
 $('.navbar-collapse ul li a').click(function() {
     $('.navbar-toggle:visible').click();
 });
+
+function stopVideo() {
+    const videoPlayer = $("#videoPlayer")[0];
+    videoPlayer.pause();
+  }
+
+function toggleButtonText() {
+    const buttonText = $("#buttonText"); // Convert to jQuery object
+    if (buttonText.text() === "Close") {
+      buttonText.text("Okay");
+    } else {
+      buttonText.text("Close");
+    }
+  
+    stopVideo();
+  }
+
+  // Attach the click event to the button
+$(document).ready(function () {
+    $("#toggleButton").click(function () {
+      toggleButtonText();
+    });
+  });
--- js/function.js
+++ js/function.js
@@ -2,3 +2,14 @@ function stopVideo() {
     const videoPlayer = document.getElementById("videoPlayer");
     videoPlayer.pause();
   }
+
+function toggleButtonText() {
+    const buttonText = document.getElementById("buttonText");
+    if (buttonText.textContent === "Close") {
+      buttonText.textContent = "Okay";
+    } else {
+      buttonText.textContent = "Close";
+    }
+    
+    stopVideo();
+  }
