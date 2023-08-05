3eaba831bd49d316bc8cb99a5a24fe301e9e727e
Hagyeong Kim <101783334+HagyeongKimDigipen@users.noreply.github.com>
2023-08-05 02:37:58 -0700

    27

--- js/function.js
+++ js/function.js
@@ -1,10 +1,12 @@
 function stopVideo() {
+  const videoPlayer = $("ortfolioModal-{{ post.modal-id }}").find("#videoPlayer")[0];
   videoPlayer.pause();
 
     // Manually close the modal
   $("#portfolioModal-{{ post.modal-id }}").modal("hide");
 }
 
+function stopVideos(button) {
   const modalId = $(button).data("id");
   const videoPlayer = $("#portfolioModal-" + modalId).find("#videoPlayer")[0];
   videoPlayer.pause();
