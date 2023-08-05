e0b41aa840336ca7208af60f6af3d7b88a42258c
Hagyeong Kim <101783334+HagyeongKimDigipen@users.noreply.github.com>
2023-08-05 01:35:03 -0700

    23

--- js/function.js
+++ js/function.js
@@ -3,26 +3,9 @@ function stopVideo() {
   videoPlayer.pause();
 
     // Manually close the modal
-    $("#portfolioModal-{{ post.modal-id }}").modal("hide");
+  $("#portfolioModal-{{ post.modal-id }}").modal("hide");
 }
 
-function toggleButtonText() {
-  const buttonText = $("#buttonText"); // Convert to jQuery object
-  if (buttonText.text() === "Close") {
-    buttonText.text("Okay");
-  } else {
-    buttonText.text("Close");
-  }
-
-  stopVideo();
-}
-
-// Attach the click event to the button
-$(document).ready(function () {
-  $("#toggleButton").click(function () {
-    toggleButtonText();
-  });
-});
 
 $(document).ready(function () {
   $("#pauseVideo").click(function () {
