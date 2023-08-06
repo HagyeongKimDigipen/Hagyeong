b16528406158bf36302f80f0b7f163338a00c7f0
Hagyeong Kim <101783334+HagyeongKimDigipen@users.noreply.github.com>
2023-08-06 00:32:27 -0700

    65

--- js/function.js
+++ js/function.js
@@ -42,58 +42,15 @@ function changeLanguage()
 }
 
 $(function() {
-  $('.change-language a').bind('click', function(event) 
+  $('.modalLanguage a').bind('click', function(event) 
   {
       var $anchor = $(this);
+
       const postModalId = $(this).data("modal-id");
-      //console.debug("modal ID is " + postModalId);
-
-      changeLanguage()
-
-      const name = $("#nameForLanguage");
-
-      const aboutLeft = $("#aboutLeft");
-      const aboutRight = $("#aboutRight");
-
-      const BSEdu = $("#BSForLanguage");
-      const eduName = $("#SchoolForLanguage");
-      const eduDate = $("#BSForLanguage span");
-
-      const BSEduKor = $("#BSForLanguageKor");
-      const eduNameKor = $("#SchoolForLanguageKor");
-      const eduDateKor = $("#SchoolForLanguageKor span");
-
-      const work = $("#workForLanguage");
-      const workDescription = $("#workDescriptionForLanguage");
-      const workDate = $("#workForLanguage span");
-
       const description = $("#descriptionForLanguage");
 
       if (window.forLanguage === true)  //english use
       {
-        $(this).text("한국어");
-        name.text("Hagyeong Kim");
-
-        aboutLeft.text("From a young age, I've been captivated by computers and dreamed of becoming a programmer. Pursuing this passion, I went to the United States to study computer. During my time at college, I delved into various programming projects, creating games and tackling diverse challenges. Overcoming numerous hurdles in programming, I found joy in problem-solving.");
-        aboutRight.text("While my focus was primarily on game and graphics engineering at school, my curiosity extends to exploring and embracing fresh challenges. Programming is the inspiration driving me to explore and learn. Thank you for visiting my portfolio, where I hope you see my unwavering enthusiasm and commitment.");
-        
-        BSEdu.text("BS in Computer Science in Real-Time Interactive Simulation");
-        eduName.text("Digipen Institue of Technology");
-        eduDate.text("2018 - 2023");
-
-        BSEduKor.text("");
-        eduNameKor.text("");
-        eduDateKor.text("");
-
-        work.text("Mickey Music Academy");
-        workDescription.text("I worked as a piano assistant instructor at a music academy, instructing numerous students ranging from kindergarten to middle school. <br>Additionally, I assisted with various administrative tasks at the academy.");
-        workDate.text("2020 - 2021");
-
-        //descriptionOutput.textContent = "{{ post.description }}";
-        
-        //$(".descriptionForLanguageEng").show();
-        //$(".descriptionForLanguageKor").hide();
-
         //start to description
         if(postModalId == "portfolioModal-1")
         {
@@ -147,24 +104,6 @@ $(function() {
       } 
       else  //korean use
       {
-        $(this).text("English");
-        name.text("김하경");
-
-        aboutLeft.text("한국어 설명이 곧 추가 될 예정입니다.");
-        aboutRight.text("한국어 설명이 곧 추가 될 예정입니다.");
-
-        BSEdu.text("BS in Computer Science in Real-Time Interactive Simulation");
-        eduName.text("Digipen Institue of Technology");
-        eduDate.text("2018 - 2023");
-
-        BSEduKor.text("디지펜게임공학과");
-        eduNameKor.text("계명대학교");
-        eduDateKor.text("2018 - 2023");
-
-        work.text("미키 음악 학원");
-        workDescription.text("음악 학원에서 피아노 보조 강사로 일했습니다, 유치부 부터 중등부까지 많은 학생들의 방과후 활동을 지도하였고 부가적으로 학원의 업무를 보조 하였습니다.");
-        workDate.text("2020 - 2021");
-
         //start to description
         if(postModalId == "portfolioModal-1")
         {
@@ -214,7 +153,79 @@ $(function() {
         {
           description.text("byebye");
         }
-        //end description
+      }
+      event.preventDefault();
+  });
+});
+
+$(function() {
+  $('.change-language a').bind('click', function(event) 
+  {
+      var $anchor = $(this);
+
+      changeLanguage()
+
+      const name = $("#nameForLanguage");
+
+      const aboutLeft = $("#aboutLeft");
+      const aboutRight = $("#aboutRight");
+
+      const BSEdu = $("#BSForLanguage");
+      const eduName = $("#SchoolForLanguage");
+      const eduDate = $("#BSForLanguage span");
+
+      const BSEduKor = $("#BSForLanguageKor");
+      const eduNameKor = $("#SchoolForLanguageKor");
+      const eduDateKor = $("#SchoolForLanguageKor span");
+
+      const work = $("#workForLanguage");
+      const workDescription = $("#workDescriptionForLanguage");
+      const workDate = $("#workForLanguage span");
+
+      if (window.forLanguage === true)  //english use
+      {
+        $(this).text("한국어");
+        name.text("Hagyeong Kim");
+
+        aboutLeft.text("From a young age, I've been captivated by computers and dreamed of becoming a programmer. Pursuing this passion, I went to the United States to study computer. During my time at college, I delved into various programming projects, creating games and tackling diverse challenges. Overcoming numerous hurdles in programming, I found joy in problem-solving.");
+        aboutRight.text("While my focus was primarily on game and graphics engineering at school, my curiosity extends to exploring and embracing fresh challenges. Programming is the inspiration driving me to explore and learn. Thank you for visiting my portfolio, where I hope you see my unwavering enthusiasm and commitment.");
+        
+        BSEdu.text("BS in Computer Science in Real-Time Interactive Simulation");
+        eduName.text("Digipen Institue of Technology");
+        eduDate.text("2018 - 2023");
+
+        BSEduKor.text("");
+        eduNameKor.text("");
+        eduDateKor.text("");
+
+        work.text("Mickey Music Academy");
+        workDescription.text("I worked as a piano assistant instructor at a music academy, instructing numerous students ranging from kindergarten to middle school. <br>Additionally, I assisted with various administrative tasks at the academy.");
+        workDate.text("2020 - 2021");
+
+        //descriptionOutput.textContent = "{{ post.description }}";
+        
+        //$(".descriptionForLanguageEng").show();
+        //$(".descriptionForLanguageKor").hide();
+      } 
+      else  //korean use
+      {
+        $(this).text("English");
+        name.text("김하경");
+
+        aboutLeft.text("한국어 설명이 곧 추가 될 예정입니다.");
+        aboutRight.text("한국어 설명이 곧 추가 될 예정입니다.");
+
+        BSEdu.text("BS in Computer Science in Real-Time Interactive Simulation");
+        eduName.text("Digipen Institue of Technology");
+        eduDate.text("2018 - 2023");
+
+        BSEduKor.text("디지펜게임공학과");
+        eduNameKor.text("계명대학교");
+        eduDateKor.text("2018 - 2023");
+
+        work.text("미키 음악 학원");
+        workDescription.text("음악 학원에서 피아노 보조 강사로 일했습니다, 유치부 부터 중등부까지 많은 학생들의 방과후 활동을 지도하였고 부가적으로 학원의 업무를 보조 하였습니다.");
+        workDate.text("2020 - 2021");
       }
 
       //showDescription();
